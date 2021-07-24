bd028889dad42c2b70cec64b9ae3cf762c032d58
PhilominaCA <35137186+PhilominaCA@users.noreply.github.com>
2021-07-24 14:04:13 +0530

    JS task - MRF functions on Rest API - 24/07
    JS task - MRF functions on Rest API - 24/07

---
+++ MRF_rest_coutries/script.js
@@ -0,0 +1,18 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.eu/rest/v2/all', true);
+request.send();
+request.onload = function() {
+    var data = JSON.parse(this.response);
+    //Get all the countries in Asia region using filter
+    var asia_countries = data.filter((elem) => elem.region === "Asia").map((elem) => elem.name);
+    console.log(asia_countries);
+    //Get all the countries with population< 2lac  using filter
+    var Ppulation = data.filter((elem) => elem.population < 200000).map((elem) => elem.name);
+    console.log(Ppulation);
+    //Print countries with currency = USD
+    var Currenc = data.filter((elem) => elem.currencies[0].code === "USD").map((elem) => elem.name);
+    console.log(Currenc);
+    //print total population of all contries using reduce
+    var total_popualtion = data.reduce((accum, elem) => { return accum + elem.population }, 0);
+    console.log(total_popualtion);
+}
